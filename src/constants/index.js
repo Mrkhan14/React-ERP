5444f97c1dd08920a8a10ae0d754fc8f038b7256
Abdulakhadkhan <50317423+Mrkhan14@users.noreply.github.com>
2024-11-27 17:34:37 +0500

    new project

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+const SKILLS = 'skills';
+const USER = 'user';
+const ENDPOINT = 'https://ap-portfolio-backend.up.railway.app/';
+// const ENDPOINT = "http://localhost:5000/";
+const TOKEN = 'PORTFOLIO_TOKEN';
+const LIMIT = 10;
+
+export { ENDPOINT, LIMIT, SKILLS, TOKEN, USER };
